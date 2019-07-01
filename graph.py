b2c6c4f1a1f4524c44e353fc2135161b68381bdf
Amr Hossam <amrhossam97@gmail.com>
2019-07-01 20:26:35 +0200

    drawing the input

---
+++ graph.py
@@ -0,0 +1,20 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+G=nx.Graph()
+f = open("input.txt", "r")
+f.readline()
+for x in f:
+    edge = x.split(" ")
+    G.add_edge(int(edge[0]),int(edge[1]),weight=edge[2])
+f.close()
+a = list(G.nodes)
+# print(a)
+# print(G.nodes.data())
+pos=nx.spring_layout(G)
+labels = nx.get_edge_attributes(G,'weight')
+nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+#nx.draw_networkx_nodes(G,pos)
+nx.draw(G,pos,node_color="blue", node_size=300, cmap=plt.cm.Blues, with_labels=True, font_weight='bold', font_color = 'red')
+#nx.draw(G, with_labels=True, font_weight='bold')
+#plt.savefig("path_graph1.png")
+plt.show()
